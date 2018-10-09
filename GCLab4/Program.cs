8ae2dd0c0ea19d668875712e555b5c44e9e6fff9
Madelyn Hilty <madelynhilty@quickenloans.com>
2018-10-09 15:51:04 -0400

    firstcommit

---
+++ GCLab4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCLab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                // user prompt - enter in an integer
+                
+
+                Console.WriteLine("Learn your squares and cubes!");
+                Console.WriteLine(" ");                
+                Console.WriteLine("Enter an integer: ");
+                var userInput = Console.ReadLine();
+
+                double userNumber;
+
+                if (double.TryParse(userInput, out userNumber))
+                {
+                    int userInteger = Convert.ToInt32(userNumber);
+
+                    int i = 1;
+
+                    Console.WriteLine("{0,-10:0}{1,-15:0}{2, -20:0 }", "Number", "Squared", "Cubed");
+                    Console.WriteLine("{0,-10:0}{1,-15:0}{2, -20:0 }", "======", "======", "======");
+
+                    while (i <= userInteger)
+                    {
+                        Console.WriteLine("{0,-10:0}{1,-15:0}{2, -20:0 }", i, i * i, i * i * i);
+                        i = i + 1;
+                    }
+
+                    Console.WriteLine("Would you like to continue? Y/N");
+                    string continueInput = Console.ReadLine();
+
+                    if (continueInput.ToLower() == "n")
+                    {
+                        break;
+                    }
+
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("That is not a valid input. Please try again.");
+                    continue;
+                }
+
+                }
+
+            }
+        }
+    }
+
